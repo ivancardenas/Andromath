f57da6ddd469d1028bbdff524d208234bf8ede04
Iván Cárdenas <ivan.cardenas@ceiba.com.co>
2018-09-25 07:43:49 -0500

    Single variable activity.

---
+++ app/src/main/java/co/edu/eafit/andromath/singlevar/SingleVariableLandingActivity.java
@@ -0,0 +1,15 @@
+package co.edu.eafit.andromath.singlevar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.edu.eafit.andromath.R;
+
+public class SingleVariableLandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_variable_landing);
+    }
+}
