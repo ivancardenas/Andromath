e88ae1787e7ebc737a7a30e5cc2c1a065f5240ee
Iván Cárdenas <ivan.cardenas@ceiba.com.co>
2018-09-25 22:34:56 -0500

    Incremental search method.

--- app/src/main/java/co/edu/eafit/andromath/singlevar/SingleVariableElectionActivity.java
+++ app/src/main/java/co/edu/eafit/andromath/singlevar/SingleVariableElectionActivity.java
@@ -1,5 +1,6 @@
 package co.edu.eafit.andromath.singlevar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,18 +8,27 @@ import android.view.View;
 import java.util.Objects;
 
 import co.edu.eafit.andromath.R;
+import co.edu.eafit.andromath.singlevar.methods.IncrementalSearchActivity;
 
 public class SingleVariableElectionActivity extends AppCompatActivity {
+
+    private Intent intent;
+
+    private String function;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_variable_election);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        function = getIntent().getStringExtra("equation");
     }
 
     public void incrementalSearch(View v) {
-
+        intent = new Intent(this, IncrementalSearchActivity.class);
+        intent.putExtra("equation", function);
+        startActivity(intent);
     }
 
     public void bisection(View v) {
