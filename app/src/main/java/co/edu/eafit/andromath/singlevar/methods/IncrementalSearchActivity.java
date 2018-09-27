69d19107abd797f640db54fe00417a822eccaf17
Iván Cárdenas <ivan.cardenas@ceiba.com.co>
2018-09-27 10:30:12 -0500

    Create procedure table.

--- app/src/main/java/co/edu/eafit/andromath/singlevar/methods/IncrementalSearchActivity.java
+++ app/src/main/java/co/edu/eafit/andromath/singlevar/methods/IncrementalSearchActivity.java
@@ -1,32 +1,42 @@
 package co.edu.eafit.andromath.singlevar.methods;
 
-import android.content.Context;
+import static co.edu.eafit.andromath.util.Constants.ErrorCodes.INVALID_DELTA;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.Gravity;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.udojava.evalex.Expression;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import co.edu.eafit.andromath.R;
 import co.edu.eafit.andromath.util.Constants;
 import co.edu.eafit.andromath.util.Messages;
 
-import static com.udojava.evalex.Expression.e;
+import static co.edu.eafit.andromath.util.Constants.ErrorCodes.INVALID_ITER;
+import static co.edu.eafit.andromath.util.Constants.ErrorCodes.X_ROOT;
 
 public class IncrementalSearchActivity extends AppCompatActivity {
 
     private static final String tag = IncrementalSearchActivity.class.getSimpleName();
 
+    TextView function, result, iterations, xValue, solution, error;
     EditText x0Input, deltaInput, iterationsInput;
-    TextView function, result;
+    TableLayout procedure;
     Expression expression;
+
+    private List<TableRow> tableIterations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +48,10 @@ public class IncrementalSearchActivity extends AppCompatActivity {
         deltaInput = (EditText) findViewById(R.id.editTextDelta);
         iterationsInput = (EditText) findViewById(R.id.editTextIterations);
 
-        result = (TextView)findViewById(R.id.textViewResult);
+        result = (TextView) findViewById(R.id.textViewResult);
         function = (TextView) findViewById(R.id.textViewFunction);
+
+        procedure = (TableLayout) findViewById(R.id.tableLayoutProcedure);
     }
 
     @Override
@@ -53,6 +65,8 @@ public class IncrementalSearchActivity extends AppCompatActivity {
 
         expression = new Expression(intent.
                 getStringExtra(Constants.EQUATION));
+
+        procedure.setStretchAllColumns(true);
     }
 
     public void incrementalSearch(View v) {
@@ -62,19 +76,32 @@ public class IncrementalSearchActivity extends AppCompatActivity {
         iterationsInput.setSelected(false);
         result.setVisibility(View.VISIBLE);
 
-        String solution = incrementalSearch();
+        tableIterations = new ArrayList<>();
+        procedure.removeViews(1, procedure.getChildCount() - 1);
+
+        Pair<String, Boolean> solution = incrementalSearch();
 
         if (solution != null) {
-            result.setText(solution);
+            result.setText(solution.first);
+            createTableProcedure(tableIterations);
+            procedure.setVisibility(solution.second ?
+                    View.VISIBLE : View.INVISIBLE);
         } else {
             Messages.invalidEquation(tag,
                     getApplicationContext());
         }
     }
 
-    private String incrementalSearch() {
+    /**
+     * @return Pair<String, Boolean>
+     *     String parameter is the message.
+     *     Boolean parameter is a flag to show the procedure.
+     */
+    private Pair<String, Boolean> incrementalSearch() {
 
-        String result;
+        String message;
+
+        boolean displayProcedure;
 
         BigDecimal x0 = BigDecimal.valueOf(Double.
                 parseDouble(x0Input.getText().toString()));
@@ -85,19 +112,21 @@ public class IncrementalSearchActivity extends AppCompatActivity {
         int iterations = Integer.parseInt(
                 iterationsInput.getText().toString());
 
-        if (delta.compareTo(BigDecimal.ZERO) == 0) {
-            result = "Delta is not valid";
-        } else {
-            if (iterations < 1) {
-                result = "Wrong number of iterations";
+        try {
+            if (delta.compareTo(BigDecimal.ZERO) == 0) {
+                message = INVALID_DELTA.getMessage();
+                displayProcedure = INVALID_DELTA.isDisplayProcedure();
             } else {
-
-                try {
+                if (iterations < 1) {
+                    message = INVALID_ITER.getMessage();
+                    displayProcedure = INVALID_ITER.isDisplayProcedure();
+                } else {
                     BigDecimal y0 = expression.with(
                             Constants.VARIABLE, x0).eval();
 
                     if (y0.compareTo(BigDecimal.ZERO) == 0) {
-                        result = "x = " + x0.toString() + " is a root";
+                        message = X_ROOT.getMessage();
+                        displayProcedure = X_ROOT.isDisplayProcedure();
                     } else {
                         BigDecimal x1 = x0.add(delta);
                         BigDecimal y1 = expression.with(
@@ -107,6 +136,9 @@ public class IncrementalSearchActivity extends AppCompatActivity {
 
                         while (y1.compareTo(BigDecimal.ZERO) != 0 && y0.multiply(y1).
                                 compareTo(BigDecimal.ZERO) > 0 && count < iterations) {
+
+                            tableIterations.add(createProcedureRow(count, x1, y0, y1));
+
                             x0 = x1;
                             y0 = y1;
                             x1 = x0.add(delta);
@@ -116,21 +148,62 @@ public class IncrementalSearchActivity extends AppCompatActivity {
                         }
 
                         if (y1.compareTo(BigDecimal.ZERO) == 0) {
-                            result = "x = " + x1.toString() + " is a root";
+                            tableIterations.add(createProcedureRow(count, x0, x1, y1));
+                            message = "x = " + x1.toString() + " is a root";
+                            displayProcedure = true;
                         } else if (y0.multiply(y1).compareTo(BigDecimal.ZERO) < 0) {
-                            result = "There is a root between x0 = "
-                                    + x0.toString() + " and x1 = " + x1.toString();
+                            message = "There is a root between x0 = " + x0.
+                                    toString() + " and x1 = " + x1.toString();
+                            displayProcedure = true;
                         } else {
-                            result = "The method failed after " + iterations + " iterations";
+                            message = "The method failed after "
+                                    + iterations + " iterations";
+                            displayProcedure = false;
                         }
                     }
-                } catch (Expression.ExpressionException
-                        | ArithmeticException | NumberFormatException e) {
-                    return null;
                 }
             }
+        } catch (Expression.ExpressionException
+                | ArithmeticException | NumberFormatException e) {
+            return null; // The equation is not valid.
         }
 
-        return result;
+        return new Pair(message, displayProcedure);
+    }
+
+    private TableRow createProcedureRow(int count, BigDecimal x1,
+                                        BigDecimal y0, BigDecimal y1) {
+
+        TableRow iterationResult = new TableRow(this);
+
+        iterations = new TextView(this);
+        iterations.setGravity(Gravity.CENTER);
+        iterations.setText(String.valueOf(count));
+
+        xValue = new TextView(this);
+        xValue.setGravity(Gravity.CENTER);
+        xValue.setText(x1.toString());
+
+        solution = new TextView(this);
+        solution.setGravity(Gravity.CENTER);
+        solution.setText(y1.toString());
+
+        error = new TextView(this);
+        error.setGravity(Gravity.CENTER);
+        error.setText(y1.subtract(y0).toString());
+
+        iterationResult.addView(iterations);
+        iterationResult.addView(xValue);
+        iterationResult.addView(solution);
+        iterationResult.addView(error);
+
+        return iterationResult;
+    }
+
+    private void createTableProcedure(List<TableRow> tableIterations) {
+
+        for (TableRow tableRow : tableIterations) {
+            procedure.addView(tableRow);
+        }
     }
 }
