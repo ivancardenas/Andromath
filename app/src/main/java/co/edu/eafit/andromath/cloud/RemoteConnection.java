28e3b69b67dc7a1d9236c1ef06779d418bb9adbd
Iván Cárdenas <ivan.cardenas@ceiba.com.co>
2018-11-25 22:21:56 -0500

    Remote procedure call.

--- app/src/main/java/co/edu/eafit/andromath/cloud/RemoteConnection.java
+++ app/src/main/java/co/edu/eafit/andromath/cloud/RemoteConnection.java
@@ -1,148 +1,35 @@
 package co.edu.eafit.andromath.cloud;
 
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
+import android.os.StrictMode;
 
-import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.QueueingConsumer;
-
-import java.net.URISyntaxException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
+import com.rabbitmq.client.RpcClient;
 
 public class RemoteConnection {
 
-    private BlockingDeque<String> queue = new LinkedBlockingDeque<String>();
-    ConnectionFactory factory = new ConnectionFactory();
-
     public RemoteConnection() {
 
-        setupConnectionFactory();
-        publishToAMQP();
+        StrictMode.ThreadPolicy policy = new StrictMode.
+                ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
-        publishMessage("hello_world");
-
-        final Handler incomingMessageHandler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                String message = msg.getData().getString("msg");
-                StringBuilder tv = new StringBuilder();
-                Date now = new Date();
-                SimpleDateFormat ft = new SimpleDateFormat("hh:mm:ss");
-                tv.append(ft.format(now) + ' ' + message + '\n');
-                System.out.println("Mensaje---------------- " + tv.toString());
-            }
-        };
-
-        subscribe(incomingMessageHandler);
-    }
-
-
-
-    void publishMessage(String message) {
         try {
-            Log.d("", "[q] " + message);
-            queue.putLast(message);
-        } catch (InterruptedException e) {
+            String uri = "amqp://sbvfmqsr:TyV5Xs3YxndY8n-jqCIM4eDhFQgqM7gW@otter.rmq.cloudamqp.com/sbvfmqsr";
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setUri(uri);
+
+            Connection conn = factory.newConnection();
+            Channel ch = conn.createChannel();
+            RpcClient service = new RpcClient(ch, "", "Hello");
+
+            System.out.println(service.stringCall("Here is the equation"));
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("Main thread caught exception: " + e);
             e.printStackTrace();
         }
-    }
-
-
-    private void setupConnectionFactory() {
-        String uri = "amqp://sbvfmqsr:TyV5Xs3YxndY8n-jqCIM4eDhFQgqM7gW@otter.rmq.cloudamqp.com/sbvfmqsr";
-        try {
-            factory.setAutomaticRecoveryEnabled(false);
-            factory.setUri(uri);
-        } catch (KeyManagementException | NoSuchAlgorithmException | URISyntaxException e1) {
-            e1.printStackTrace();
-        }
-    }
-
-    public void publishToAMQP() {
-        Thread publishThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        Connection connection = factory.newConnection();
-                        Channel ch = connection.createChannel();
-                        ch.confirmSelect();
-
-                        while (true) {
-                            String message = queue.takeFirst();
-                            try {
-                                ch.basicPublish("amq.fanout", "chat", null, message.getBytes());
-                                Log.d("", "[s] " + message);
-                                ch.waitForConfirmsOrDie();
-                            } catch (Exception e) {
-                                Log.d("", "[f] " + message);
-                                queue.putFirst(message);
-                                throw e;
-                            }
-                        }
-                    } catch (InterruptedException e) {
-                        break;
-                    } catch (Exception e) {
-                        Log.d("", "Connection broken: " + e.getClass().getName());
-                        try {
-                            Thread.sleep(5000); //sleep and then try again
-                        } catch (InterruptedException e1) {
-                            break;
-                        }
-                    }
-                }
-            }
-        });
-        publishThread.start();
-    }
-
-    void subscribe(final Handler handler)
-    {
-        Thread subscribeThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(true) {
-                    try {
-                        Connection connection = factory.newConnection();
-                        Channel channel = connection.createChannel();
-                        channel.basicQos(1);
-                        AMQP.Queue.DeclareOk q = channel.queueDeclare();
-                        channel.queueBind(q.getQueue(), "amq.fanout", "chat");
-                        QueueingConsumer consumer = new QueueingConsumer(channel);
-                        channel.basicConsume(q.getQueue(), true, consumer);
-
-                            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-                            String message = new String(delivery.getBody());
-                            Log.d("","[r] " + message);
-                            Message msg = handler.obtainMessage();
-                            Bundle bundle = new Bundle();
-                            bundle.putString("msg", message);
-                            msg.setData(bundle);
-                            handler.sendMessage(msg);
-
-                    } catch (InterruptedException e) {
-                        break;
-                    } catch (Exception e1) {
-                        Log.d("", "Connection broken: " + e1.getClass().getName());
-                        try {
-                            Thread.sleep(5000); //sleep and then try again
-                        } catch (InterruptedException e) {
-                            break;
-                        }
-                    }
-                }
-            }
-        });
-        subscribeThread.start();
     }
 }
